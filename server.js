0fbd2c94a7935f1359948bbd19261747e60b0ac6
FelipeRude <felipe.rude@hs-weingarten.de>
2024-10-26 14:06:13 +0200

    Comments in JSON Datei

--- server.js
+++ server.js
@@ -1,47 +1,67 @@
 const express = require('express');
-const path = require('path');  // Für den einfachen Dateipfadzugriff
+const path = require('path');
+const fs = require('fs'); // Modul für Dateizugriff
 const app = express();
 const PORT = 2000;
-// Array zum Speichern der Kommentare
-const comments = [];
+
 // Middleware zum Parsen von JSON
 app.use(express.json());
-// "static" als statisches Verzeichnis festlegen
+
+// Statisches Verzeichnis festlegen
 app.use(express.static('static'));
 
-// Route für die Startseite festlegen
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'static', 'index.html'));
-});
+// Datei-Pfad für die Kommentare
+const commentsFilePath = path.join(__dirname, 'comments.json');
+
+// Funktion zum Laden der Kommentare aus der JSON-Datei
+function loadComments() {
+  try {
+    const data = fs.readFileSync(commentsFilePath, 'utf-8');
+    return JSON.parse(data);
+  } catch (error) {
+    // Falls die Datei nicht existiert oder fehlerhaft ist, geben wir ein leeres Array zurück
+    return [];
+  }
+}
+
+// Funktion zum Speichern der Kommentare in der JSON-Datei
+function saveComments(comments) {
+  fs.writeFileSync(commentsFilePath, JSON.stringify(comments, null, 2));
+}
+
 // POST-Endpunkt zum Speichern des Kommentars
 app.post('/submit-comment', (req, res) => {
-    const { name, comment } = req.body;
-  
-    if (name && comment) {
-      // Kommentar ins Array hinzufügen
-      comments.push({ name, comment });
-      res.json({ message: 'Kommentar erfolgreich gespeichert!' });
-    } else {
-      res.status(400).json({ message: 'Name und Kommentar sind erforderlich!' });
-    }
-  });
-app.get('/comments', (req, res) => {
-    res.json(comments);
+  const { name, comment } = req.body;
+
+  if (name && comment) {
+    // Kommentare laden, neuen Kommentar hinzufügen und speichern
+    const comments = loadComments();
+    comments.push({ name, comment });
+    saveComments(comments);
+
+    res.json({ message: 'Kommentar erfolgreich gespeichert!' });
+  } else {
+    res.status(400).json({ message: 'Name und Kommentar sind erforderlich!' });
+  }
 });
+
+// GET-Endpunkt, um alle Kommentare zurückzugeben
+app.get('/comments', (req, res) => {
+  const comments = loadComments();
+  res.json(comments);
+});
+
+// Routen für die verschiedenen Seiten
 app.get('/index', (req, res) => {
   res.sendFile(path.join(__dirname, 'static', 'index.html'));
 });
-  
+
 app.get('/page1', (req, res) => {
   res.sendFile(path.join(__dirname, 'static', 'page1.html'));
 });
 
 app.get('/page2', (req, res) => {
   res.sendFile(path.join(__dirname, 'static', 'page2.html'));
-});
-
-app.get('/page3', (req, res) => {
-  res.sendFile(path.join(__dirname, 'static', 'page3.html'));
 });
 
 // Server starten
